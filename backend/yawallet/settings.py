d3a53d2acf312f7b1942a270a34d9010b6c7933e
apiload5 <amir03182055632@gmail.com>
2026-07-17 05:09:06 +0500

    Update settings.py

--- backend/yawallet/settings.py
+++ backend/yawallet/settings.py
@@ -42,6 +42,25 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# ============================================
+# TEMPLATES - ADD THIS COMPLETE SECTION
+# ============================================
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = 'yawallet.urls'
 WSGI_APPLICATION = 'yawallet.wsgi.application'
 
